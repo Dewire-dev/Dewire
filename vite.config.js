3e510fd604cba5a9ae8f1b08c5608cc0149ad9e2
AnaelBonnafous <anael.bonnafous@gmail.com>
2023-03-26 01:08:23 +0100

    feat(import): add unplugin auto-import libraries + path alias to js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
+import path from "path";
+
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+
+import Icons from 'unplugin-icons/vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
 
 export default defineConfig({
     plugins: [
@@ -16,7 +22,29 @@ export default defineConfig({
                 },
             },
         }),
+        Icons({}),
+        AutoImport({
+            imports: [
+                'vue',
+                {
+                    '@inertiajs/vue3': [
+                        'useForm', 'usePage', 'useRemember', 'router',
+                    ],
+                },
+            ],
+            dirs: ['resources/js/Composables'],
+            dts: 'resources/js/auto-imports.d.ts',
+        }),
+        Components({
+            dirs: ['resources/js/Components', 'resources/js/Layouts'],
+            dts: 'resources/js/components.d.ts',
+        }),
     ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./resources/js"),
+        },
+    },
     server: {
         hmr: {
             host: 'localhost',
