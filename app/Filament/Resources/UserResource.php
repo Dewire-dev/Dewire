9a5a38c03ca7ff16769efc9c2f1feef8f0bbb221
AnaelBonnafous <anael.bonnafous@gmail.com>
2023-03-28 16:17:42 +0200

    fix(backoffice): adjust user resource fields and icon

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -18,13 +18,15 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-users';
+    protected static ?string $navigationIcon = 'heroicon-o-user';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\TextInput::make('firstname'),
+                Forms\Components\TextInput::make('lastname'),
                 Forms\Components\TextInput::make('email')->email()->required(),
                 Forms\Components\TextInput::make('password')->password()->required(),
             ]);
