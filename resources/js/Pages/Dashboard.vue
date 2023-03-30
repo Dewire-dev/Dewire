0ebdc5e11a826b1844de2d2cea8d4f0d5b708b9b
AnaelBonnafous <anael.bonnafous@gmail.com>
2023-03-30 19:57:42 +0200

    fix(dashboard): add title to dashboard page

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -13,6 +13,13 @@ defineProps<{
 
 <template>
     <AppLayout title="Dashboard">
+        <template #header>
+            <div class="">
+                <h2 class="text-xl font-semibold text-gray-800 dark:text-gray-200 text-center">
+                    Dashboard
+                </h2>
+            </div>
+        </template>
         <div class="grid grid-cols-3 gap-4 mb-4">
             <div
                 class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800"
@@ -55,19 +62,6 @@ defineProps<{
                 class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800"
             >
                 <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
-        <template #header>
-            <BreadCrumb  />
-            <div class="">
-                <h2 class="text-xl font-semibold text-gray-800 dark:text-gray-200 text-center">Accueil</h2>
-            </div>
-        </template>
-
-        <div class="py-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <h3 class="text-lg text-gray-800 dark:text-gray-200">
-                Mes projets
-            </h3>
-            <div class="grid grid-cols-3 gap-6 mx-6 mt-12">
-                <ProjectCard v-for="project in projects" :project="project" />
             </div>
         </div>
     </AppLayout>
