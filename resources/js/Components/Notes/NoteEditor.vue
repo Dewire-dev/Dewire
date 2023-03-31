241aba6ed35f6bd33eeb97764865559fc6f6ae53
AnaelBonnafous <anael.bonnafous@gmail.com>
2023-03-31 12:42:02 +0200

    feat(notes): add editable toggle to note editor

--- resources/js/Components/Notes/NoteEditor.vue
+++ resources/js/Components/Notes/NoteEditor.vue
@@ -29,17 +29,27 @@ watch(
     }
 );
 
+const isEditable = ref(true);
+
+watch(isEditable, (value) => {
+    editor.value?.setEditable(value);
+});
+
 onBeforeUnmount(() => {
     editor.value?.destroy();
 });
 </script>
 
 <template>
-    <editor-content :editor="editor" class="editor" />
+    <div class="p-4 bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
+        <div>
+            <input
+                type="checkbox"
+                :checked="isEditable"
+                @change="() => (isEditable = !isEditable)"
+            />
+            Editable
+        </div>
+        <editor-content :editor="editor" />
+    </div>
 </template>
-
-<style scoped lang="scss">
-.editor {
-    @apply bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white
-}
-</style>
