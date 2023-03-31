8f6e0d7e6987d16634f0ae0fe8998c2a60b3208a
AnaelBonnafous <anael.bonnafous@gmail.com>
2023-03-31 10:55:10 +0200

    feat(layout): add projects to global data + typing

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,6 +1,6 @@
-<script setup>
-import {ref} from 'vue';
+<script setup lang="ts">
 import {Head, Link, router} from '@inertiajs/vue3';
+import route from 'ziggy-js';
 import {useDark} from "@vueuse/core";
 import ApplicationMark from '@/Components/ApplicationMark.vue';
 import Banner from '@/Components/Banner.vue';
@@ -14,6 +14,8 @@ const isDark = useDark();
 defineProps({
     title: String,
 });
+
+const projects = computed<App.Models.Project[]>(() => usePage().props.layout?.projects || [])
 
 const showingNavigationDropdown = ref(false);
 
@@ -123,7 +125,7 @@ const logout = () => {
                 <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-sidebar">
                     <ul class="space-y-2 font-medium">
                         <li>
-                            <Link :class="route().current('dashboard') ? 'dark:nav-item-active nav-item-active' : ''" :href="route('projects.index')"
+                            <Link :class="route().current('dashboard') ? 'dark:nav-item-active nav-item-active' : ''" :href="route('dashboard')"
                                   class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 tab-projects">
                                 <i-carbon-home class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i-carbon-home>
                                 <span class="flex-1 ml-3 whitespace-nowrap">Accueil</span>
