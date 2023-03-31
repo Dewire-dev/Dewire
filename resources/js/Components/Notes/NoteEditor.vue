104d170232ab1c692045bf64bdc3690793902f52
AnaelBonnafous <anael.bonnafous@gmail.com>
2023-03-31 14:12:22 +0200

    feat(notes): add floating menu with h1, h2 and ul tags

--- resources/js/Components/Notes/NoteEditor.vue
+++ resources/js/Components/Notes/NoteEditor.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { useEditor, EditorContent } from "@tiptap/vue-3";
+import { useEditor, EditorContent, FloatingMenu } from "@tiptap/vue-3";
 import StarterKit from "@tiptap/starter-kit";
+import { Button } from "flowbite-vue";
 
 const props = withDefaults(
     defineProps<{
@@ -55,6 +56,42 @@ onBeforeUnmount(() => {
             />
             Editable
         </div>
+        <FloatingMenu
+            v-if="editor"
+            :editor="editor"
+            :tippy-options="{ duration: 100 }"
+            class="flex gap-2"
+        >
+            <Button
+                color="light"
+                @click="
+                    editor?.chain().focus().toggleHeading({ level: 1 }).run()
+                "
+                :class="{
+                    'is-active': editor.isActive('heading', { level: 1 }),
+                }"
+            >
+                H1
+            </Button>
+            <Button
+                color="light"
+                @click="
+                    editor?.chain().focus().toggleHeading({ level: 2 }).run()
+                "
+                :class="{
+                    'is-active': editor.isActive('heading', { level: 2 }),
+                }"
+            >
+                H2
+            </Button>
+            <Button
+                color="light"
+                @click="editor?.chain().focus().toggleBulletList().run()"
+                :class="{ 'is-active': editor.isActive('bulletList') }"
+            >
+                Bullet List
+            </Button>
+        </FloatingMenu>
         <editor-content :editor="editor" />
     </div>
 </template>
