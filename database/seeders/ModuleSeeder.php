a016dc8c67d8eed6147b54fd6986e9088bcefc50
AnaelBonnafous <71386173+AnaelBonnafous@users.noreply.github.com>
2023-04-21 10:36:34 +0200

    feat(modules): add color to module seeder

--- database/seeders/ModuleSeeder.php
+++ database/seeders/ModuleSeeder.php
@@ -17,34 +17,42 @@ class ModuleSeeder extends Seeder
             [
                 'name' => 'Chat',
                 'description' => 'Envoi de messages en temps réel via un salon de discussion.',
+                'color' => '#e91e63',
             ],
             [
                 'name' => 'Email',
                 'description' => 'Envoi et lecture de messages via E-mail interne et SMTP.',
+                'color' => '#2196f3',
             ],
             [
                 'name' => 'File',
                 'description' => 'Stockage et gestion de fichiers.',
+                'color' => '#795548',
             ],
             [
                 'name' => 'Kanban',
                 'description' => 'Gestion de projet via un tableau kanban avec drag and drop.',
+                'color' => '#ffc107',
             ],
             [
                 'name' => 'List',
                 'description' => 'Listes de choses à acheter.',
+                'color' => '#9c27b0',
             ],
             [
                 'name' => 'Note',
                 'description' => 'Prise de note en temps réel via éditeur de texte.',
+                'color' => '#4caf50',
             ],
             [
                 'name' => 'Planning',
                 'description' => 'Calendrier pour afficher des évènements.',
+                'color' => '#3f51b5',
             ],
             [
                 'name' => 'Time',
                 'description' => 'Estimation de temps de travail avec compteur.',
+                'color' => '#f44336',
             ],
         ];
 
