20e8f958a1e60fc918f3eec526c1acfc704ab0f7
AnaelBonnafous <anael.bonnafous@gmail.com>
2023-06-20 12:44:29 +0200

    fix(layouts): home link go to dashboard

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -126,7 +126,7 @@ const logout = () => {
                 <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-sidebar">
                     <ul class="space-y-2 font-medium">
                         <li>
-                            <Link :class="route().current('dashboard') ? 'dark:nav-item-active nav-item-active' : ''" :href="route('projects.index')"
+                            <Link :class="route().current('dashboard') ? 'dark:nav-item-active nav-item-active' : ''" :href="route('dashboard')"
                                   class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 tab-projects">
                                 <i-carbon-home class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i-carbon-home>
                                 <span class="flex-1 ml-3 whitespace-nowrap">Accueil</span>
