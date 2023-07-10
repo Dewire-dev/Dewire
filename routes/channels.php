03b79afa486ad317814cfdc37a2cc136933024a3
AnaelBonnafous <anael.bonnafous@gmail.com>
2023-07-11 00:57:51 +0200

    fix(note): get note id parameter as string

--- routes/channels.php
+++ routes/channels.php
@@ -18,6 +18,6 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('note.{id}', function (User $user, int $id) {
+Broadcast::channel('note.{id}', function (User $user, string $id) {
     return ['id' => $user->id, 'name' => $user->name];
 });
