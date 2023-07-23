cecd99014cbfbd309d18c7d03c6774ae7ab59768
AnaelBonnafous <anael.bonnafous@gmail.com>
2023-07-23 22:18:09 +0200

    feat(notes): redesign bubble menu

--- resources/js/Components/Notes/NoteEditor/EditorBubbleMenu.vue
+++ resources/js/Components/Notes/NoteEditor/EditorBubbleMenu.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Editor, BubbleMenu } from "@tiptap/vue-3";
+import { Badge } from "flowbite-vue";
 
 defineProps<{
     editor?: Editor;
@@ -10,41 +11,26 @@ defineProps<{
     <BubbleMenu
         v-if="editor"
         :editor="editor"
-        :tippy-options="{ duration: 100 }"
+        :tippy-options="{ duration: 100, placement: 'top-start' }"
+        class="flex"
     >
-        <span
-            @click="editor.chain().focus().toggleBold().run()"
-            class="badge"
-            :class="{ active: editor.isActive('bold') }"
+        <Badge
+            @click.prevent="editor.chain().focus().toggleBold().run()"
+            :type="editor.isActive('bold') ? 'default' : 'dark'"
         >
-            bold
-        </span>
-        <span
+            <i-carbon-text-bold />
+        </Badge>
+        <Badge
             @click="editor.chain().focus().toggleItalic().run()"
-            class="badge"
-            :class="{ active: editor.isActive('italic') }"
+            :type="editor.isActive('italic') ? 'default' : 'dark'"
         >
-            italic
-        </span>
-        <span
+            <i-carbon-text-italic />
+        </Badge>
+        <Badge
             @click="editor.chain().focus().toggleStrike().run()"
-            class="badge"
-            :class="{ active: editor.isActive('strike') }"
+            :type="editor.isActive('strike') ? 'default' : 'dark'"
         >
-            strike
-        </span>
+            <i-carbon-text-strikethrough />
+        </Badge>
     </BubbleMenu>
 </template>
-
-<style scoped lang="scss">
-.badge {
-    @apply text-xs font-medium mr-2 px-2.5 py-0.5 rounded;
-    &.active {
-        @apply bg-blue-100 text-blue-800 dark:bg-blue-700 dark:text-blue-300;
-    }
-    &:not(.active) {
-        @apply bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300;
-    }
-}
-</style>
-
